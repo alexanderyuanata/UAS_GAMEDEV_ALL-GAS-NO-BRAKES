f06fc16aeb83010c075c4c464faa74fb2390e2c2
phen28 <92217368+phen28@users.noreply.github.com>
2023-05-15 12:33:41 +0700

    update

--- UAS_GAMEDEV/Assets/Scripts/PlayerMovement.cs
+++ UAS_GAMEDEV/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
-    public float speed = 12f;
+    float speed;
+    public float walking_speed = 12f;
+    public float running_speed;
     public float gravity = -9.81f;
 
     Vector3 velocity;
@@ -16,6 +18,15 @@ public class PlayerMovement : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = running_speed;
+        }
+        else
+        {
+            speed = walking_speed;
+        }
 
         Vector3 move = transform.right * x + transform.forward * z;
 
