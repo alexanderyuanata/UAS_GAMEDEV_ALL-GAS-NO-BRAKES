a7db5e88c30eb595ea26a460d13f1607d07cad63
unfocusedlex <92197888+unfocusedlex@users.noreply.github.com>
2023-05-18 11:26:37 +0700

    pickable item rework, added more items

--- UAS_GAMEDEV/Assets/Scripts/PickableItem.cs
+++ UAS_GAMEDEV/Assets/Scripts/PickableItem.cs
@@ -7,6 +7,10 @@ public class PickableItem : InteractableObject
     [SerializeField] private Collider objcollider;
     [SerializeField] private InventoryItem item_data;
     [SerializeField] private ushort item_count;
+    [SerializeField] private string[] pickup_text;
+    [SerializeField] private bool enable_popup = false;
+    [SerializeField] private bool enable_destroy = true;
+    [SerializeField] private bool pickup_once = true;
 
     void Start()
     {
@@ -15,11 +19,16 @@ public class PickableItem : InteractableObject
 
     public override void TryInteract()
     {
-        Debug.Log(item_data.getName());
-        //try and add item, also play pickup SFX
-        PlayerInventory.instance.addItem(item_data, item_count);
-        SFXManager1.instance.playItemPickup();
-        //when picked up, fade out and destroy the item
-        Destroy(gameObject);
+        if (pickup_once)
+        {
+            //try and add item, also play pickup SFX
+            PlayerInventory.instance.addItem(item_data, item_count);
+            SFXManager1.instance.playItemPickup();
+
+            //when picked up
+            if (enable_popup) DialogueManager.instance.startDialogue(pickup_text);
+            if (enable_destroy) Destroy(gameObject);
+            pickup_once = false;
+        }
     }
 }
